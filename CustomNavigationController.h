b2f32b25d859cb250cb180c651da655e9ffd1232
Vitor Lopes Leonardi <vitor.lleonardi@gmail.com>
2011-10-23 01:55:00 -0200

    Initial commit

---
+++ CustomNavigationController.h
@@ -0,0 +1,12 @@
+//
+//  CustomNavigationController.h
+//
+//  Created by Vitor Leonardi on 10/23/11.
+//  Copyright (c) 2011 Vitor Leonardi. All rights reserved.
+//  Licensed for use and distribution.
+
+#import <Foundation/Foundation.h>
+
+@interface CustomNavigationController : UINavigationController
+
+@end
---
+++ CustomNavigationController.m
@@ -0,0 +1,27 @@
+//
+//  CustomNavigationController.m
+//
+//  Created by Vitor Leonardi on 10/23/11.
+//  Copyright (c) 2011 Vitor Leonardi. All rights reserved.
+//  Licensed for use and distribution.
+
+
+#import "CustomNavigationController.h"
+
+@implementation CustomNavigationController
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // --- Tests if it is the iOS 5
+        if ([self.navigationBar respondsToSelector:@selector(setBackgroundImage:forBarMetrics:)]) {
+            [self.navigationBar setBackgroundImage:[UIImage imageNamed:@"Put Your Image Here"] forBarMetrics:UIBarMetricsDefault];
+
+            // --- Optionaly sets the color of the navigation buttons
+            UIColor *color = [UIColor colorWithRed:(100/255.0f) green:(100/255.0f) blue:(100/255.0f) alpha:1.0f];
+            self.navigationBar.tintColor = color;
+        }
+    }
+    return self;
+}
+@end
